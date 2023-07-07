f75cc1ca89d2cbbf7392e7cf43c0a29018c7c70c
reptilianIX <reptlianIX@gmail.com>
2023-07-07 14:05:32 +0300

    hw_3 done

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -2,15 +2,28 @@
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
 
+SELECT customers.company_name, CONCAT(first_name, ' ', last_name) as full_name FROM orders
+INNER JOIN customers USING(customer_id)
+INNER JOIN employees USING(employee_id)
+WHERE customers.city='London' AND employees.city = 'London' AND ship_via = 2
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
 
+SELECT product_name, units_in_stock, suppliers.contact_name, suppliers.phone FROM products
+INNER JOIN suppliers USING(supplier_id)
+WHERE discontinued = 0 AND units_in_stock < 25 AND category_id in (2,4)
+ORDER BY units_in_stock ASC
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
 
+SELECT company_name FROM customers
+WHERE NOT EXISTS (SELECT * FROM orders WHERE customers.customer_id = orders.customer_id)
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+
+SELECT DISTINCT product_name FROM products
+WHERE EXISTS (SELECT * FROM order_details WHERE products.product_id=order_details.product_id AND quantity = 15)
